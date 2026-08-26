df59d2c89fe7ad9202d108d79dc9b839b9542cd7
Pranali9708 <pranali.rane@turtlemint.com>
2026-08-26 09:13:59 +0530

    added changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -47,7 +47,11 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      // use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { width: 1920, height: 1080 },
+      },
     },
 
     // {
