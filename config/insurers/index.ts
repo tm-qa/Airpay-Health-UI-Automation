5953af4ebc45107f81cf7ed90fbba6de576956a3
Pranali9708 <pranali.rane@turtlemint.com>
2026-08-26 09:51:07 +0530

    runing only hdfc health

--- config/insurers/index.ts
+++ config/insurers/index.ts
@@ -2,8 +2,10 @@ import { HDFC_CONFIG } from "./hdfc.config";
 import { ICICI_CONFIG } from "./icici.config";
 import { InsurerConfig } from "./types";
 
-/** Add new insurer config here — tests auto-register from this list */
-export const HEALTH_INSURERS: InsurerConfig[] = [HDFC_CONFIG, ICICI_CONFIG];
+
+// export const HEALTH_INSURERS: InsurerConfig[] = [HDFC_CONFIG, ICICI_CONFIG];
+export const HEALTH_INSURERS: InsurerConfig[] = [HDFC_CONFIG,];
+
 
 export { HDFC_CONFIG } from "./hdfc.config";
 export { ICICI_CONFIG } from "./icici.config";
